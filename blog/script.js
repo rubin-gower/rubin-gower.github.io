c583649cb1092a72d9d002660f9a8c8d7e25cec1
rubin-gower <rubinsemails@gmail.com>
2020-04-22 16:11:23 +1200

    added easter egg

--- blog/script.js
+++ blog/script.js
@@ -6,3 +6,17 @@ function notready() {
 	alert("that blog is not ready yet ):")
 	
 }
+function easteregg() {
+
+wait = setInterval(rainbow,100);
+
+}
+function rainbow() {
+	console.log("runing rainbow")
+	var red = Math.floor(Math.random() * 255);
+	var blue = Math.floor(Math.random() * 255);
+	var green = Math.floor(Math.random() * 255);
+document.getElementById("thetitle").style.color = "rgb(" + red + "," + blue + "," + green + ")";
+}
+	var wait = 0; 
+
